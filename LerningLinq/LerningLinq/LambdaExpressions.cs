8987ecb79958a2e01a21bcef5fe116d9d4fcdb2f
Marta Rudnik <marta.rudnik@intive.com>
2020-09-15 01:51:29 +0200

    A lambda expression that will return the next number after the provided integer

---
+++ LerningLinq/LerningLinq/LambdaExpressions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LerningLinq
+{
+    public static class LambdaExpressions
+    {
+        //// A lambda expression that will return the next number after the provided integer
+        public static Func<int, int> GetNextNumber = num => num+1;
+    }
+}
--- LerningLinq/LerningLinq/Program.cs
+++ LerningLinq/LerningLinq/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LerningLinq
 {
@@ -10,6 +6,9 @@ namespace LerningLinq
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(LambdaExpressions.GetNextNumber(6));
+
+            Console.ReadLine();
         }
     }
 }
